08c59c3242ed865d316711203831e35ac097c3c5
liang <807311214@qq.com>
2025-05-19 10:51:20 +0800

    增加go test

---
+++ golang/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var myMutex sync.Mutex
+
+var myCount int
+
+func increment() {
+	myMutex.Lock()
+	fmt.Println(myCount)
+	myMutex.Unlock()
+
+}
+
+func main() {
+	myCount = 0
+	// 创建三个通道用于同步控制
+	// chanA := make(chan int) recv和send的返回是在对面收到后返回
+	chanA := make(chan int, 1)
+	chanB := make(chan int, 1)
+	chanC := make(chan int, 1)
+	var wg sync.WaitGroup
+
+	print := func(ch <-chan int, next chan<- int, char string) {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			<-ch            // 等待信号
+			fmt.Print(char) // 打印字符
+			next <- i
+			// 通知下一个协程
+		}
+	}
+
+	increment()
+
+	wg.Add(3)
+	go print(chanA, chanB, "a")
+	go print(chanB, chanC, "b")
+	go print(chanC, chanA, "c")
+
+	// 启动协程链
+	chanA <- 1 // 触发第一个协程
+	wg.Wait()
+	fmt.Println()
+}
